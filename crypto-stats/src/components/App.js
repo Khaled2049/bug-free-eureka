4c57ea96164b98145857a1464a2916290984cf0f
Khaled Hossain <khaledhossain.not@gmail.com>
2022-06-20 17:47:14 -0500

    Added header

--- crypto-stats/src/components/App.js
+++ crypto-stats/src/components/App.js
@@ -1,7 +1,12 @@
 import './App.css';
-
+import React from 'react';
+import Header from './Header';
 function App() {
-  return <div className="App">TEST</div>;
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
 }
 
 export default App;
