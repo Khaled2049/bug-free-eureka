8bf2973a6a0ea8e6b804734a0e1e7383fcbc09ea
Khaled Hossain <khaledhossain.not@gmail.com>
2022-06-20 18:13:52 -0500

    Added hero

--- crypto-stats/src/components/App.js
+++ crypto-stats/src/components/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import React from 'react';
 import Header from './Header';
+import Hero from './Hero';
+
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Hero />
     </React.Fragment>
   );
 }
---
+++ crypto-stats/src/components/Hero/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import heroBg from '../../assets/hero-bg.png';
+
+const Hero = () => {
+  return (
+    <div
+      style={{ backgroundImage: `url(${heroBg})` }}
+      className="px-4 text-center border-bottom"
+    >
+      <div className="overflow-hidden">
+        <div className="container">
+          <img width="300px" src={require('../../assets/redLogo.png')}></img>
+        </div>
+      </div>
+      <h1 className="display-4 fw-bold">CRYPTO STATS</h1>
+
+      <div className="col-lg-6 mx-auto">
+        <p className="lead mb-4">
+          Know what's happening and never miss your moonshot
+        </p>
+        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
+          <Button variant="danger">SET ALERTS NOW</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
