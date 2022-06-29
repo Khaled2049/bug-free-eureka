a91e8f935407d7be0b10a83761227d777c0324ca
Khaled Hossain <khaledhossain.not@gmail.com>
2022-06-29 18:55:15 -0500

    Added cards

--- merch/src/pages/Store.tsx
+++ merch/src/pages/Store.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
-
+import { Row, Col } from 'react-bootstrap';
+import storeItems from '../data/items.json';
+import StoreItem from '../components/StoreItem';
 const Store = () => {
-  return <div>Store</div>;
+  const renderStoreItems = () => {
+    return storeItems.map((item) => {
+      return (
+        <Col key={item.id}>
+          <StoreItem {...item} />
+        </Col>
+      );
+    });
+  };
+
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {renderStoreItems()}
+      </Row>
+    </>
+  );
 };
 
 export default Store;
