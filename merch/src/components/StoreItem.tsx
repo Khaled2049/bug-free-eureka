2d7ae18235055ebd63579103f0cb68dc131ede15
Khaled Hossain <khaledhossain.not@gmail.com>
2022-07-01 19:30:52 -0500

    Added quantity

--- merch/src/components/StoreItem.tsx
+++ merch/src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
+import { useShoppingCart } from '../context/ShoppingCartContext';
 import { formatCurrency } from '../utils/formatCurrency';
 
 type StoreItemProps = {
@@ -10,7 +11,13 @@ type StoreItemProps = {
 };
 
 const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
-  const quantity: number = 0;
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+  const quantity = getItemQuantity(id);
 
   return (
     <Card className="h-100">
@@ -27,7 +34,9 @@ const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
         </Card.Title>
         <div className="mt-auto">
           {quantity === 0 ? (
-            <Button className="w-100">+ Add to cart</Button>
+            <Button className="w-100" onClick={() => increaseCartQuantity(id)}>
+              + Add to cart
+            </Button>
           ) : (
             <div
               className="d-flex align-items-center justify-content-center flex-column"
@@ -37,13 +46,17 @@ const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
                 className="d-flex align-items-center justify-content-center"
                 style={{ gap: '.5rem' }}
               >
-                <Button>-</Button>
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
                 <div>
                   <span className="sf-3">{quantity}</span> in card
                 </div>
-                <Button>+</Button>
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
               </div>
-              <Button variant="danger" size="sm">
+              <Button
+                onClick={() => removeFromCart(id)}
+                variant="danger"
+                size="sm"
+              >
                 Remove Items
               </Button>
             </div>
--- merch/src/context/ShoppingCartContext.tsx
+++ merch/src/context/ShoppingCartContext.tsx
@@ -24,8 +24,57 @@ export function useShoppingCart() {
 
 export function ShoppingCartProvider({ children }: ShoppingCartProvideProps) {
   const [cartItems, setcartItems] = useState<CartItem[]>([]);
+  const getItemQuantity = (id: number) => {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  };
+
+  const increaseCartQuantity = (id: number) => {
+    setcartItems((curItems) => {
+      if (curItems.find((item) => item.id === id) == null) {
+        return [...curItems, { id, quantity: 1 }];
+      } else {
+        return curItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  };
+
+  const decreaseCartQuantity = (id: number) => {
+    setcartItems((curItems) => {
+      if (curItems.find((item) => item.id === id)?.quantity === 1) {
+        return curItems.filter((item) => item.id !== id);
+      } else {
+        return curItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  };
+
+  const removeFromCart = (id: number) => {
+    setcartItems((currItems) => {
+      return currItems.filter((item) => item.id !== id);
+    });
+  };
+
   return (
-    <ShoppingCartContext.Provider value={{}}>
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+      }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
