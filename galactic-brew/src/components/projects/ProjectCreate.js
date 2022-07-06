417ebf26ba24167cda45abc983f9ed83b850448f
Khaled Hossain <khaledhossain.not@gmail.com>
2022-07-06 11:16:41 -0500

    Added redux-form

--- galactic-brew/src/components/projects/ProjectCreate.js
+++ galactic-brew/src/components/projects/ProjectCreate.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const ProjectCreate = () => {
-  return <div>ProjectCreate</div>;
+class ProjectCreate extends React.Component {
+  renderInput = ({ input, label, meta }) => {
+    // console.log(formProps);
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    console.log(className);
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  renderError = ({ touched, error }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form
+        className="error ui form"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'Enter a title';
+  }
+  if (!formValues.description) {
+    errors.description = 'Enter a Description';
+  }
+  return errors;
 };
 
-export default ProjectCreate;
+export default reduxForm({
+  form: 'projectCreate',
+  validate,
+})(ProjectCreate);
--- galactic-brew/src/reducers/index.js
+++ galactic-brew/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import { newsReducer } from './newsReducer';
 import authReducer from './authReducer';
 
@@ -24,4 +25,5 @@ export default combineReducers({
   selectedSong: selectedSongReducer,
   news: newsReducer,
   auth: authReducer,
+  form: formReducer,
 });
