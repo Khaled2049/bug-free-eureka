bb2c74e0c615f8c42c11b1c7ea458e5e19a8fa23
Khaled Hossain <khaledhossain.not@gmail.com>
2022-07-30 16:37:41 -0500

    Add header

--- ecom/pages/index.tsx
+++ ecom/pages/index.tsx
@@ -3,22 +3,38 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { Product, HeroBanner, FooterBanner } from '../components';
+import { client } from '../lib/client';
 
-const Home: NextPage = () => {
+interface IHomeProps {
+  products: any[];
+  bannerData: any[];
+}
+
+const Home = ({ products, bannerData }: IHomeProps) => {
+  // console.log('Products', products);
+  console.log('BannerData', bannerData);
   return (
     <>
-      <HeroBanner></HeroBanner>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}></HeroBanner>
       <div className="products-heading">
         <h2>Best selling Products</h2>
         <p>Buy art from local artists</p>
       </div>
 
       <div className="products-container">
-        {['art', 'art2'].map((product) => product)}
+        {products.map((product) => product)}
       </div>
       <FooterBanner></FooterBanner>
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "products"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+  return { props: { products, bannerData } };
 };
 
 export default Home;
