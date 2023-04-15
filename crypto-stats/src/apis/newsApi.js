2a9b50ca8023cf65bcaf3c6aa3f7bb23d1757262
Khaled <khaledhossain.not@gmail.com>
2023-04-14 18:42:55 -0600

    remove old projects

--- crypto-stats/src/apis/newsApi.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://newsapi.org/v2',
-  headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_NEWSAPIKEY}`,
-  },
-});
--- merch/src/api/unsplash.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization: `Client-ID ${import.meta.env.VITE_UNSPLASH_API}`,
-  },
-});
